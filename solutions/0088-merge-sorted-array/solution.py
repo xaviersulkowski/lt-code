ce04d36fd8f9b34d17295bf57407ca4fb4f65819
xavier.sulkowski <xaviersulkowski@gmail.com>
2026-01-24 19:22:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.4 MB (13.85%)

--- solutions/0088-merge-sorted-array/solution.py
+++ solutions/0088-merge-sorted-array/solution.py
@@ -1,23 +1,26 @@
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
+        Do not return anything, modify nums1 in-place instead.
         """
 
-        i, j, write_i = m-1, n-1, m+n-1
+        # m is a pointer at latest real num 1 
+        # n is a pointer at latest num 2 
 
-        while j >= 0: 
-            if i >= 0 and nums1[i] > nums2[j]:
-                nums1[write_i] = nums1[i]
-                i -= 1
+        # m_ insert pointer
+        m_ = m + n 
+
+        while m > 0 and n > 0: 
+            if nums1[m - 1] < nums2[n - 1]: 
+                nums1[m_ - 1] = nums2[n - 1]
+                n -= 1 
             else: 
-                nums1[write_i] = nums2[j]
-                j -= 1
+                nums1[m_ - 1] = nums1[m - 1]
+                m -= 1
+            m_ -= 1
 
-            write_i -= 1
-            
+        while n > 0:
+            nums1[m_ -1] = nums2[n - 1]
+            n -= 1
+            m_ -= 1
 
