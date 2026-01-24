5d6f52e775e58010af4f8bf5333afa16f2db85d0
xavier.sulkowski <xaviersulkowski@gmail.com>
2026-01-24 19:49:00 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.4 MB (20.21%)

--- solutions/0027-remove-element/solution.py
+++ solutions/0027-remove-element/solution.py
@@ -1,21 +1,14 @@
-class Solution(object):
-    def removeElement(self, nums, val):
-        """
-        :type nums: List[int]
-        :type val: int
-        :rtype: int
-        """
-        
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+
+        # pointer at nums where to put the non-val value
+        k = 0
         i = 0
-        nums_len = len(nums)
-        removed_elements = 0
+        while i < len(nums):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+            i += 1
 
-        while i < nums_len: 
-            if nums[i] == val: 
-                nums.pop(i)
-                nums_len -= 1
-                removed_elements += 1
-            else:
-                i += 1
+        return k
 
-        return len(nums)
