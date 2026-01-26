9d884e2f818faab8769c21e79d9738df6ab67c78
xavier.sulkowski <xaviersulkowski@gmail.com>
2026-01-26 16:27:30 +0000

    [LeetCode Sync] Runtime - 7 ms (49.26%), Memory - 26.6 MB (24.74%)

---
+++ solutions/0189-rotate-array/solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) < 2:
+            return nums
+
+        k = k % len(nums) # 2 / 5 = 3 -> need to move 3 elements
+        l, r = 0, len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
+
+        l, r = 0, k - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
+
+        l, r = k, len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
