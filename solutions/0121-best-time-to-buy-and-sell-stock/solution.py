8388a688bd7e463bb7fa2bcac492626780554f4f
xavier.sulkowski <xaviersulkowski@gmail.com>
2026-01-26 16:58:24 +0000

    [LeetCode Sync] Runtime - 75 ms (49.80%), Memory - 28.9 MB (9.43%)

--- solutions/0121-best-time-to-buy-and-sell-stock/solution.py
+++ solutions/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,13 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-
-        buy_price = prices[0]        
+        l_ptr, r_ptr = 0, 1
         profit = 0
-        
-        for p in prices[1:]: 
-            if p < buy_price: 
-                buy_price = p 
 
-            profit = max(profit, p - buy_price)
+        while r_ptr < len(prices):
+            tmp_profit = prices[r_ptr] - prices[l_ptr]
+            if prices[r_ptr] < prices[l_ptr]:
+                l_ptr = r_ptr
+            else:
+                profit = max(profit, tmp_profit)
+            r_ptr += 1
 
         return profit
+
