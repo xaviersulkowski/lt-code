16f7075f88058bc9e8c5fe75d61d447bc4a7c8ba
xavier.sulkowski <xaviersulkowski@gmail.com>
2026-01-27 21:45:54 +0000

    [LeetCode Sync] Runtime - 7 ms (57.96%), Memory - 21.3 MB (8.78%)

--- solutions/0169-majority-element/solution.py
+++ solutions/0169-majority-element/solution.py
@@ -1,15 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         # boyer-moore voting https://www.geeksforgeeks.org/theory-of-computation/boyer-moore-majority-voting-algorithm/
+        
         res = nums[0]
         count = 1
-        for i in range(1, len(nums)):
-            if nums[i] == res:
+        for i in range(1, len(nums)): 
+            if nums[i] == res: 
                 count += 1
-            else:
-                count -= 1
-                if count == 0:
+            else: 
+                count -= 1 
+                if count <= 0: 
                     res = nums[i]
-                    count = 1
-
+                    count += 1 
         return res
