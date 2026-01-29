5312e14f3e50b5f6d299dfb00ff391c26877751d
xavier.sulkowski <xaviersulkowski@gmail.com>
2026-01-29 21:00:13 +0000

    [LeetCode Sync] Runtime - 3 ms (26.02%), Memory - 19.4 MB (28.86%)

---
+++ solutions/0274-h-index/solution.py
@@ -0,0 +1,47 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        # #####################
+        #   sorting solution  #
+        # #####################
+
+        # if not citations:
+        #     return 0
+        #
+        # x = sorted(citations) # O(n log n) time + O(n) space
+        # n = len(x)
+        #
+        # for i in range(n): # O(n)
+        #     h = n - i
+        #     if x[i] >= h:
+        #         return h
+        #
+        # return 0
+        #
+        # total time O(n log n)
+        # total space O(n)
+
+        # #####################
+        #  counting solution  #
+        # #####################
+
+        n = len(citations)
+        papers_count = [0] * (n + 1) # this creates an array of zeros; each position/index is a count of citations
+        # [count of articles with O citations, count of articles with 1 citation, count of articles with 2 citations, etc]
+        # this is O(n) space
+
+        for c in citations: # this is O(n) time
+            papers_count[min(c, n)] += 1
+
+        print(f"papers count: {papers_count}")
+
+        h = n
+        papers = papers_count[n]
+        while papers < h:
+            print(f"h={h}; papers count: {papers}")
+            h -= 1
+            papers += papers_count[h]
+
+        # total time: O(n)
+        # total space: O(n)
+
+        return h
