326b86ec4c458acf5f9aa75fafddb9a016d6eebc
xavier.sulkowski <xaviersulkowski@gmail.com>
2026-02-01 19:59:58 +0000

    [LeetCode Sync] Runtime - 4 ms (70.96%), Memory - 21.2 MB (24.34%)

--- solutions/0169-majority-element/solution.py
+++ solutions/0169-majority-element/solution.py
@@ -3,13 +3,14 @@ class Solution:
         # boyer-moore voting https://www.geeksforgeeks.org/theory-of-computation/boyer-moore-majority-voting-algorithm/
         
         res = nums[0]
-        count = 1
+        count = 1 
         for i in range(1, len(nums)): 
             if nums[i] == res: 
-                count += 1
+                count += 1 
             else: 
                 count -= 1 
                 if count <= 0: 
                     res = nums[i]
-                    count += 1 
+                    count = 1
+        
         return res
