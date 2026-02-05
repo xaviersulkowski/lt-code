73857ca53deb59016148d35d931b6dde394b116e
xavier.sulkowski <xaviersulkowski@gmail.com>
2026-02-05 16:18:41 +0000

    [LeetCode Sync] Runtime - 6 ms (89.82%), Memory - 19.6 MB (15.09%)

--- solutions/0006-zigzag-conversion/solution.py
+++ solutions/0006-zigzag-conversion/solution.py
@@ -1,18 +1,35 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-
         if numRows == 1: 
-            return s
-
-        rows = [''] * min(numRows, len(s))
+            return s 
         
-        row, desc = 0, False
+        # Approach 1: step 
+        # res = ""
+        # step = 2 * (numRows - 1)
+        
+        # for r in range(numRows): 
+        #     for i in range(r, len(s), step):
+        #         res += s[i]
+        #         if r > 0 and r < numRows -1 and (i + step - 2 * r ) < len(s): 
+        #             res += s[i + step - 2 * r ]
+        
+        # return res
 
-        for c in s: 
-            rows[row] += c
-            if row == 0 or row == numRows - 1: 
-                desc = not desc 
-            row += 1 if desc else -1 
-
-        return ''.join(rows)
-
+        # Approach 2: fill in rows 
+        res = [""] * numRows
+        going_down = True
+        row = 0
+        for ch in s: 
+            res[row] += ch
+            
+            if row == 0: 
+                going_down = True 
+            elif row == numRows - 1: 
+                going_down = False 
+            
+            if going_down: 
+                row += 1 
+            else: 
+                row -= 1
+        
+        return "".join(res)
