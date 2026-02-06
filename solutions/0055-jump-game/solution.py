fc65c9d2761971dee6494ce7cdb2bbd7ded96b90
xavier.sulkowski <xaviersulkowski@gmail.com>
2026-02-06 20:29:57 +0000

    [LeetCode Sync] Runtime - 4 ms (99.18%), Memory - 20.8 MB (7.69%)

--- solutions/0055-jump-game/solution.py
+++ solutions/0055-jump-game/solution.py
@@ -1,9 +1,10 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         # greedy 
-        goal = len(nums) - 1 
-        for i in range(len(nums) -1, -1, -1): 
+        goal = len(nums) - 1
+        for i in range(len(nums) - 2, -1, -1): 
             if i + nums[i] >= goal: 
                 goal = i 
-        
-        return goal == 0
+            
+        return True if goal == 0 else False 
+
