93748102d6fa6901771e7d827aa120d0d1c80cc2
xavier.sulkowski <xaviersulkowski@gmail.com>
2026-02-09 21:39:40 +0000

    [LeetCode Sync] Runtime - 3 ms (80.32%), Memory - 20.6 MB (46.66%)

--- solutions/0167-two-sum-ii---input-array-is-sorted/solution.py
+++ solutions/0167-two-sum-ii---input-array-is-sorted/solution.py
@@ -1,17 +1,15 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         l, r = 0, len(numbers) - 1
-        
-        while l < r: 
-            numl = numbers[l]
-            numr = numbers[r]
 
-            if numl + numr == target: 
+        while l < r: 
+            tmp_sum = numbers[l] + numbers[r] 
+            if tmp_sum == target: 
                 return [l + 1, r + 1]
             
-            if numr + numl > target: 
+            if tmp_sum > target: 
                 r -= 1 
-
-            if numr + numl < target: 
-                l += 1
+            
+            if tmp_sum < target: 
+                l += 1 
 
