2f16f7c27ce5b2d6292d878eb0a4a204154a3633
xavier.sulkowski <xaviersulkowski@gmail.com>
2026-02-13 22:57:07 +0000

    [LeetCode Sync] Runtime - 63 ms (77.03%), Memory - 19.8 MB (27.20%)

--- solutions/0076-minimum-window-substring/solution.py
+++ solutions/0076-minimum-window-substring/solution.py
@@ -3,29 +3,35 @@ class Solution:
         if len(t) > len(s) or s == "":
             return ""
         
-        window, counts = {}, {} 
+        window, counts = {}, {}
 
         for x in t: 
             counts[x] = 1 + counts.get(x, 0)
         
-        have, need = 0, len(counts)
+        need, have = len(counts), 0
+
         res, res_len = (-1, -1), float('inf')
-        l = 0
+
+        l = 0 
         for r in range(len(s)): 
             c = s[r]
+            
             window[c] = 1 + window.get(c, 0)
             
-            if c in counts and window[c] == counts[c]:
-                have += 1 
+            if c in counts and window[c] == counts[c]: 
+                have += 1
             
             while need == have: 
+               
+                window[s[l]] -= 1
+
                 if (r - l + 1) < res_len: 
-                    res_len = r - l + 1
+                    res_len = (r - l + 1)
                     res = (l, r)
 
-                window[s[l]] -= 1 
-                if s[l] in counts and window[s[l]] < counts[s[l]]: 
-                    have -= 1
-                l += 1 
+                if s[l] in counts and window[s[l]] < counts[s[l]]:
+                    have -= 1 
+
+                l += 1
         
         return s[res[0]:res[1]+1]
