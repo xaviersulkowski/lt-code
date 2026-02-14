3e634e1a79e514f84390118c475cba7d85e73f93
xavier.sulkowski <xaviersulkowski@gmail.com>
2026-02-14 17:20:26 +0000

    [LeetCode Sync] Runtime - 26 ms (34.07%), Memory - 19.6 MB (52.20%)

--- solutions/0383-ransom-note/solution.py
+++ solutions/0383-ransom-note/solution.py
@@ -1,10 +1,14 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         
-        for rn in ransomNote: 
-            if rn not in magazine:
-                return False 
-            
-            magazine = magazine.replace(rn, '', 1)
+        magazineCounts = {}
+        for x in magazine: 
+            magazineCounts[x] = 1 + magazineCounts.get(x, 0)
+        
+        for c in ransomNote:
+            if c in magazineCounts and magazineCounts[c] > 0:
+                magazineCounts[c] -= 1 
+            else: 
+                return False
 
-        return True
+        return True 
