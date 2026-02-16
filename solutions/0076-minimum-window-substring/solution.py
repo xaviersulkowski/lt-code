05ec8fdbef8bc7c24a4e185f5b376f4c57402bb6
xavier.sulkowski <xaviersulkowski@gmail.com>
2026-02-16 21:05:36 +0000

    [LeetCode Sync] Runtime - 59 ms (86.85%), Memory - 19.7 MB (67.80%)

--- solutions/0076-minimum-window-substring/solution.py
+++ solutions/0076-minimum-window-substring/solution.py
@@ -1,37 +1,33 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(t) > len(s) or s == "":
-            return ""
-        
-        window, counts = {}, {}
-
+        counts, window = {}, {}
         for x in t: 
             counts[x] = 1 + counts.get(x, 0)
         
         need, have = len(counts), 0
 
-        res, res_len = (-1, -1), float('inf')
+        res, res_len = (), float('inf')
 
         l = 0 
         for r in range(len(s)): 
             c = s[r]
-            
             window[c] = 1 + window.get(c, 0)
-            
+
             if c in counts and window[c] == counts[c]: 
                 have += 1
-            
+
             while need == have: 
-               
-                window[s[l]] -= 1
+                c2 = s[l]
 
-                if (r - l + 1) < res_len: 
-                    res_len = (r - l + 1)
-                    res = (l, r)
+                window[c2] -= 1 
 
-                if s[l] in counts and window[s[l]] < counts[s[l]]:
+                if c2 in counts and window[c2] < counts[c2]:
                     have -= 1 
+                
+                if r - l + 1 < res_len:
+                    res_len = r - l + 1 
+                    res = (l, r)
+                
+                l += 1 
 
-                l += 1
-        
-        return s[res[0]:res[1]+1]
+        return "" if res_len == float('inf') else s[res[0]:res[1]+1]
