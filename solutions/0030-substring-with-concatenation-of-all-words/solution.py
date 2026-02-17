13dc6f316ac12f837ee6db6ecf4578d7fcba4db6
xavier.sulkowski <xaviersulkowski@gmail.com>
2026-02-17 20:00:17 +0000

    [LeetCode Sync] Runtime - 23 ms (77.50%), Memory - 19.9 MB (65.51%)

--- solutions/0030-substring-with-concatenation-of-all-words/solution.py
+++ solutions/0030-substring-with-concatenation-of-all-words/solution.py
@@ -5,35 +5,32 @@ class Solution:
         res = []
 
         for w in words: 
-            counts[w] = counts.get(w, 0) + 1 
+            counts[w] = 1 + counts.get(w, 0)
         
-        for i in range(n): 
-            start = i
-            window = {} 
+        for i in range(len(words[0])):
+            start = i 
+            window = {}
             have = 0 
 
-            for l in range(i, len(s) - n + 1, n): 
-
+            for l in range(i, len(s) - n + 1, n):
                 word = s[l:l+n]
                 window[word] = 1 + window.get(word, 0)
 
                 if word in counts: 
-                    have += 1
-                    
+                    have += 1 
+
                     while window[word] > counts[word]:
-                        first_left_str = s[start:start + n]
-                        window[first_left_str] -= 1 
+                        left_str = s[start:start+n]
+                        window[left_str] -= 1 
                         start += n 
                         have -= 1 
 
                     if have == len(words):
                         res.append(start)
-                        
 
-                else: 
+                else:
+                    have = 0 
                     window = {}
                     start = l + n
-                    have = 0
-        
-        return res
 
+        return res
