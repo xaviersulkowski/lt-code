eb5dfb4a499c2136dc068b75f46d44bc5a046e60
xavier.sulkowski <xaviersulkowski@gmail.com>
2026-02-21 21:01:39 +0000

    [LeetCode Sync] Runtime - 7 ms (54.42%), Memory - 26.5 MB (72.23%)

--- solutions/0189-rotate-array/solution.py
+++ solutions/0189-rotate-array/solution.py
@@ -1,28 +1,25 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        if len(nums) < 2:
-            return nums
-
-        k = k % len(nums)
+        k = k % len(nums) 
+        n = len(nums)
         
-        l, r = 0, len(nums) - 1
+        # reverse entire array 
+        l, r = 0, n - 1
         while l < r: 
             nums[l], nums[r] = nums[r], nums[l]
             l += 1 
             r -= 1
 
+        # reverse first part
         l, r = 0, k - 1
         while l < r: 
             nums[l], nums[r] = nums[r], nums[l]
             l += 1 
             r -= 1
 
-        l, r = k, len(nums) - 1
+        # reverse second part
+        l, r = k, n - 1
         while l < r: 
             nums[l], nums[r] = nums[r], nums[l]
             l += 1 
             r -= 1
- 
