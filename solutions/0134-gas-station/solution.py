511611c05d5d506ddc9322a552289256a2ca818f
xavier.sulkowski <xaviersulkowski@gmail.com>
2026-02-21 21:12:51 +0000

    [LeetCode Sync] Runtime - 19 ms (77.23%), Memory - 26.1 MB (56.38%)

--- solutions/0134-gas-station/solution.py
+++ solutions/0134-gas-station/solution.py
@@ -1,18 +1,17 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        if sum(cost) > sum(gas): 
+        if sum(gas) < sum(cost):
             return -1 
-        
-        tank = 0
+
+        total = 0 
         res = 0 
+
         for i in range(len(gas)): 
-            diff = gas[i] - cost[i]
-            tank += diff 
-
-            if tank < 0:
-                tank = 0
+            total += gas[i]
+            if total - cost[i] < 0 :
+                total = 0
                 res = i + 1
-
+            else: 
+                total -= cost[i]
+        
         return res
-            
-
