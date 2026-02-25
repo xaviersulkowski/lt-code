e8b5f6f3815f922eb6a990115a5d26c4dc1bca79
xavier.sulkowski <xaviersulkowski@gmail.com>
2026-02-25 21:18:58 +0000

    [LeetCode Sync] Runtime - 27 ms (85.37%), Memory - 32.4 MB (74.17%)

--- solutions/0219-contains-duplicate-ii/solution.py
+++ solutions/0219-contains-duplicate-ii/solution.py
@@ -1,9 +1,17 @@
 class Solution:
-    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:    
-        seen={}
-        for i in range(len(nums)):
-            if nums[i] in seen:
-                if i - seen[nums[i]]<=k:
-                    return True
-            seen[nums[i]]=i
-        return False
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        window = set()
+        l = 0
+
+        for r in range(len(nums)): 
+            if r - l > k: 
+                window.remove(nums[l])
+                l += 1
+            
+            if nums[r] in window: 
+                return True 
+            
+            
+            window.add(nums[r])
+
+        return False 
