2273b2e4bdbde594dc78c7770658af6e422701c0
xavier.sulkowski <xaviersulkowski@gmail.com>
2026-02-27 22:15:08 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.4 MB (66.69%)

--- solutions/0036-valid-sudoku/solution.py
+++ solutions/0036-valid-sudoku/solution.py
@@ -2,40 +2,28 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         rows = [set() for x in range(9)]
         columns = [set() for x in range(9)]
-        grids = [set() for x in range(9)] # 0, 1, 2
-                                          # 3, 4, 5, 
-                                          # 6, 7, 8 
+        groups = [set() for x in range(9)]
 
-        for r in range(9):
-            for c in range(9):
-                val = board[r][c]
-                grid_index = (r // 3) * 3 + (c // 3)
+        for i in range(9):
+            for j in range(9): 
                 
-                if val == ".":
+                num = board[i][j]
+
+                if num == ".": 
                     continue 
 
-                row = rows[r]
-                if val in row: 
+                if num in rows[i]:
                     return False
-                else:
-                    rows[r].add(val)
                 
-                column = columns[c]
-                if val in column: 
-                    return False
-                else:
-                    columns[c].add(val)
-                
-                grid = grids[grid_index]
-                if val in grid:
-                    return False
-                else: 
-                    grids[grid_index].add(val)
+                if num in columns[j]:
+                    return False 
 
+                group_key = (i // 3) * 3 + (j // 3)
+                if num in groups[group_key]:
+                    return False
+
+                rows[i].add(num)
+                columns[j].add(num)
+                groups[group_key].add(num)
+        
         return True
-                
-            
-
-
-                
-
