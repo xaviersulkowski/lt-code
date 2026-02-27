978b2066e7b7e83c19ffdbf5c2990d480db18359
xavier.sulkowski <xaviersulkowski@gmail.com>
2026-02-27 21:56:22 +0000

    [LeetCode Sync] Runtime - 7 ms (40.85%), Memory - 19.1 MB (86.54%)

--- solutions/0205-isomorphic-strings/solution.py
+++ solutions/0205-isomorphic-strings/solution.py
@@ -1,16 +1,15 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        map_s, map_t = {}, {}
+        if len(s) != len(t): 
+            return False
         
-        for x, y in zip(s, t): 
-            if (
-                (x in map_s and map_s[x] != y) or 
-                (y in map_t and map_t[y] != x)
-            ):
+        map_s = {}
+        map_t = {}
+
+        for i, j in zip(s, t): 
+            if ((i in map_s and map_s[i] != j) or (j in map_t and map_t[j] != i)):
                 return False
-            
-            map_s[x] = y
-            map_t[y] = x
-
+            map_s[i] = j 
+            map_t[j] = i
+        
         return True
-
