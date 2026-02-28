17f8a4d3f1d0f8395deb251b8b2f358355864097
xavier.sulkowski <xaviersulkowski@gmail.com>
2026-02-28 18:18:24 +0000

    [LeetCode Sync] Runtime - 23 ms (57.53%), Memory - 25.4 MB (72.59%)

--- solutions/0238-product-of-array-except-self/solution.py
+++ solutions/0238-product-of-array-except-self/solution.py
@@ -1,5 +1,23 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # input 
+        # [1, 2, 3, 4]
+
+        # init output
+        # [1, 1, 1, 1]
+        
+        # prefixes
+        # [1, 1, 2, 6]
+
+        # output left to right multiplied with prefixes
+        # [1, 1, 2, 6]
+
+        # postfixes 
+        # [1, 4, 12, 24] (right to left)
+        
+        # output right to left multiplied with postfixes
+        # [24, 12, 8, 6]
+
         output = [1] * len(nums)
         prefix = 1 
         for i in range(len(nums)): 
@@ -12,4 +30,3 @@ class Solution:
             postfix *= nums[i]
 
         return output
-
