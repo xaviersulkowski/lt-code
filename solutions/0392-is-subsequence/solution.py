f4c6b689b49741c59d4449e6a6289532b5317cbb
xavier.sulkowski <xaviersulkowski@gmail.com>
2026-03-11 19:57:34 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (60.97%)

--- solutions/0392-is-subsequence/solution.py
+++ solutions/0392-is-subsequence/solution.py
@@ -1,13 +1,12 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        if len(s) > len(t): 
-            return False 
-
+        if len(s) > len(t):
+            return False
+        
         l, p = 0, 0
-        while p < len(t) and l < len(s): 
+        while l < len(s) and p < len(t): 
             if s[l] == t[p]:
                 l += 1
             p += 1 
         
         return True if l == len(s) else False
-            
