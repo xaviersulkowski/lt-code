c229dee714a73409b5688fbf26de39ecd90c7cdc
xavier.sulkowski <xaviersulkowski@gmail.com>
2026-03-12 22:50:02 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (90.12%)

--- solutions/0290-word-pattern/solution.py
+++ solutions/0290-word-pattern/solution.py
@@ -1,19 +1,19 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
+        map_p = {}
+        map_s = {} 
 
-        map_pattern = {}
-        map_s = {}
-
-        s = s.split()
-
-        if len(s) != len(pattern): 
+        s = s.split(" ")
+        if len(s) != len(pattern):
             return False
 
         for p, c in zip(pattern, s): 
-            if (p in map_pattern and map_pattern[p] != c) or ((c in map_s and map_s[c] != p)): 
-                return False 
-            
-            map_pattern[p] = c
+            if (
+                (p in map_p and map_p[p] != c ) 
+                or (c in map_s and map_s[c] != p)
+            ): 
+                return False
+            map_p[p] = c
             map_s[c] = p
 
-        return True
+        return True 
