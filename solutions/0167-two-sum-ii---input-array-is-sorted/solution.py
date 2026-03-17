e804c4cc8f105b64708074d9b9dfef65c231361f
xavier.sulkowski <xaviersulkowski@gmail.com>
2026-03-17 21:59:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.5 MB (65.99%)

--- solutions/0167-two-sum-ii---input-array-is-sorted/solution.py
+++ solutions/0167-two-sum-ii---input-array-is-sorted/solution.py
@@ -1,10 +1,24 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        seen = {}
-        for i in range(len(numbers)): 
-            diff = target - numbers[i]
-            if diff in seen: 
-                return [seen[diff] + 1, i + 1]
-            seen[numbers[i]] = i
-        return False
-        
+        # approach 1 with seen hashmap 
+        # seen = {}
+        # for i in range(len(numbers)): 
+        #     diff = target - numbers[i]
+        #     if diff in seen: 
+        #         return [seen[diff] + 1, i + 1]
+        #     seen[numbers[i]] = i
+        # return False
+    
+        # approach 2 two pointers 
+        l = 0
+        r = len(numbers) - 1 
+
+        while l < r: 
+            sum_ = numbers[l] + numbers[r]
+            if sum_ > target: 
+                r -= 1 
+            if sum_ < target: 
+                l += 1 
+            if sum_ == target: 
+                return [l+1,r+1]
+
