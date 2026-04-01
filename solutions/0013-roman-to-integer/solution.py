124e3bebe55e1a36a872e6e0dc2e3ead7eaa86cd
xavier.sulkowski <xaviersulkowski@gmail.com>
2026-04-01 21:11:19 +0000

    [LeetCode Sync] Runtime - 7 ms (42.27%), Memory - 19.2 MB (93.82%)

--- solutions/0013-roman-to-integer/solution.py
+++ solutions/0013-roman-to-integer/solution.py
@@ -1,32 +1,27 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-
-        romanMap = {
+        symbols = {
             "I": 1,
-            "V": 5, 
+            "V": 5,
             "X": 10,
             "L": 50,
             "C": 100,
             "D": 500,
-            "M": 1000
+            "M": 1000,
         }
 
-        result = 0 
+        res = 0 
 
-        for i in range(0, len(s) - 1, 1): 
-            c1 = romanMap[s[i]]
-            c2 = romanMap[s[i+1]]
-
-            if c2 > c1: 
-                result -= c1
+        for i in range(len(s) - 1): 
+            num1 = symbols[s[i]]
+            num2 = symbols[s[i + 1]]
+            
+            if num2 > num1:
+                res -= num1
             else: 
-                result += c1 
-
-        result += romanMap[s[-1]]
-
-        return result
-
+                res += num1 
+        
+        res += symbols[s[-1]]
+        return res 
             
 
-
-        
